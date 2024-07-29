1587edb7547196f6f1250030ef71563c5f60fccf
Bloodimirg <bloodimirg@gmail.com>
2024-07-29 15:44:12 +0300

    Дописаны CBV классы для Product

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, View, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from .models import Product
+from .forms import ProductForm
 
-from catalog.models import Product
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+
+    def get_success_url(self, *args, **kwargs):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
 
 
 class ProductListView(ListView):
     model = Product
+    template_name = 'catalog/product_list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = 'catalog/product_detail.html'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
+    template_name = 'catalog/product_confirm_delete.html'
 
 
 class ContactsView(View):
@@ -23,4 +48,3 @@ class ContactsView(View):
         print(f"Имя: {name} | телефон: {phone} | сообщение: {message}")
         # Здесь можно добавить логику обработки данных формы
         return render(request, template_name='catalog/contacts.html')
-

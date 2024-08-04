61f0bb5a1491e9fbad00b91a685cfee42b6a7915
Bloodimirg <bloodimirg@gmail.com>
2024-08-05 00:00:56 +0300

    #

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('product_form/', ProductCreateView.as_view(), name='product_create'),
-    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # тоже самое что view
     path('product_update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product_delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 

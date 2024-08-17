a86ec99548f06107d8b227561d5edba6daf9d08e
Bloodimirg <bloodimirg@gmail.com>
2024-08-17 20:20:40 +0300

    добавлены пользователи в админу + поле группы

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,9 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "phone", "country", "token")
+    list_display = ("email", "phone", "country", "token", 'get_groups')
+
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
+    get_groups.short_description = 'Groups'
